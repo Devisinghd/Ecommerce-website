65d45acaa9bcee787ad37815b57b69b141e36689
devisingh dangi <devisinghdangi786@gmail.com>
2026-03-31 16:54:09 +0530

    First project commit

---
+++ myapp/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('<slug:slug>',views.detail,name='detail')
+]
---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Products
+# Create your views here.
+def index(request):
+    products = Products.objects.all()
+    return render(request,'myapp/index.html',{"products":products})
+
+def detail(request, slug):
+    product = Products.objects.get(slug=slug)
+    return render(request,'myapp/detail.html',{'product':product})
