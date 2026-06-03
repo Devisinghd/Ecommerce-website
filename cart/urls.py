9ba267902fd7c30ab386b2c7833c18e491bb1385
devisingh dangi <devisinghdangi786@gmail.com>
2026-06-03 17:35:20 +0530

    url pattern added for cart CRUD

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'cart'
+
+
 urlpatterns = [
     path('add/', views.cart_add, name='cart_add'),
-    path('cart-overview/',views.cart_overview,name='cart-overiew')
+    path('delete/',views.cart_delete,name='cart_delete'),
+    path('update/',views.cart_update,name='cart_update'),
+    path('cart-overview/',views.cart_overview,name='cart-overview'),
 ]
