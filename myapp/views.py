d2961570b802ba80b4cbe52dbdceb8afe6b15f54
devisingh dangi <devisinghdangi786@gmail.com>
2026-06-11 16:34:39 +0530

    pagination and caching

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Products
+from django.core.paginator import Paginator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 # Create your views here.
+@cache_page(60 * 15)
+@vary_on_headers("User-Agent")
 def index(request):
     products = Products.objects.all()
-    return render(request,'myapp/index.html',{"products":products})
+    paginator = Paginator(products,7)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'myapp/index.html',{"page_obj":page_obj})
 
+@cache_page(60 * 15)
 def detail(request, slug):
     product = Products.objects.get(slug=slug)
     return render(request,'myapp/detail.html',{'product':product})
