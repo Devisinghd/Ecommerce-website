b12c4e3f89a6b0fa184f5cbd91a02647cc56800b
devisingh dangi <devisinghdangi786@gmail.com>
2026-06-16 12:06:08 +0530

    palceholder  added

--- users/form.py
+++ users/form.py
@@ -3,11 +3,18 @@ from django.contrib.auth.models import User
 from django import forms
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"example@gmail.com"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Enter Password"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password"}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+        widgets = {
+            'username': forms.TextInput(attrs={"placeholder": "Enter Username"}),
+            'email': forms.EmailInput(attrs={"placeholder": "example@gmail.com"}),
+        }
 
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs)
@@ -15,9 +22,8 @@ class CreateUserForm(UserCreationForm):
             field.widget.attrs.update({'class': 'w-full rounded-lg border border-gray-300 px-3 py-2 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50'})
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput())
-
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Enter Username"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Enter Password"}))
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
         widget_attrs = {
@@ -33,3 +39,8 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username','email']
         exclude = ['password1','password2']
+
+        widgets = {
+            'username':forms.TextInput(attrs={"placeholder":"Enter Username"}),
+            'email':forms.TextInput(attrs={"placeholder":"example@gmail.com"})
+    }
