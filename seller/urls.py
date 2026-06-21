4cc4e59448eeb171b118cc88b3054ae203a88578
devisingh dangi <devisinghdangi786@gmail.com>
2026-06-21 12:15:56 +0530

    seller url.py created

--- seller/urls.py
+++ seller/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    
+    path('dashboard/', views.seller_dashboard, name='seller-dashboard'),
+    path('product/add/', views.product_create, name='seller-product-create'),
+    path('product/<slug:slug>/', views.product_detail, name='seller-product-detail'),
+    #path('product/<slug:slug>/edit/', views.product_update, name='seller-product-update'),
+    #path('product/<slug:slug>/delete/', views.product_delete, name='seller-product-delete'),
 ]
