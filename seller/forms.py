e5218b33f9574abbeaee67d5c0f7d1210ebd45e3
devisingh dangi <devisinghdangi786@gmail.com>
2026-06-21 12:17:11 +0530

    product creation form

---
+++ seller/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from myapp.models import Products
+
+class ProductCreateForm(forms.ModelForm):
+    class Meta:
+        model = Products
+        fields = ['name','price','description','image','stock','active']
+
+
+    def cleen_price(self):
+        product_price = self.cleaned_data['price']
+        if product_price < 0:
+            raise forms.ValidationError("price can not be negative")
+        return product_price
