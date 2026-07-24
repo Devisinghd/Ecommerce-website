e807a6e59074ff8aeec9298a94fd7bff7639f1e4
devisingh dangi <devisinghdangi786@gmail.com>
2026-07-24 17:54:59 +0530

    viewset used for api

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,17 @@
 
 from django.urls import path,include
 from . import views
-from django.views import View as auth_view
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'products',views.ProductViewSet,basename='product')
 
 urlpatterns = [
     path('',views.index,name='index'),
     path('<slug:slug>',views.detail,name='detail'),
 
     #API url patterns
-    path('product-api/',views.product_API,name='product-api'),
-    path('product/api/<int:pk>',views.product_detail_API,name='product-detail-api'),
+    path("api/",include(router.urls)),
+    #path('product-api/',views.ProductListCreateAPI.as_view(),name='product-api'),
+    #path('product/api/<int:pk>',views.ProductRetrieveUpdateDelete.as_view(),name='product-detail-api'),
 ]
