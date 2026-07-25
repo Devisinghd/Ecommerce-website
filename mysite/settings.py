5d519170668261948758d9b9c3eb5c63bafc40a9
devisingh dangi <devisinghdangi786@gmail.com>
2026-07-25 16:50:23 +0530

    api authentication configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework.authtoken',
     'rest_framework',
     'seller',
     "myapp",
@@ -184,3 +185,13 @@ LOGGING = {
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', # Blocks unauthenticated users globally
+    ]
+}
