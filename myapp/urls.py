1f0582899f391c2d214a91c56d90793b1c30156b
devisingh dangi <devisinghdangi786@gmail.com>
2026-08-01 18:01:31 +0530

    url pattern for api's

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,13 @@
 from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import   TokenObtainPairView,TokenRefreshView
 
 router = DefaultRouter()
-router.register(r'products',views.ProductViewSet,basename='product')
+router.register(r'products', views.ProductViewSet, basename='product')
+router.register(r'orders', views.OrderViewSet, basename='order')
+router.register(r'order-items', views.OrderItemViewSet, basename='orderitem')
+router.register(r'addresses', views.AddressViewSet, basename='address')
 
 urlpatterns = [
     path('',views.index,name='index'),
@@ -12,4 +16,6 @@ urlpatterns = [
 
     #API url patterns
     path("api/",include(router.urls)),
+    path("api/token",TokenObtainPairView.as_view()),
+    path("api/token/refresh",TokenRefreshView.as_view()),
 ]
