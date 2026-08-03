b9d5e9c50f14567536370a6943fcd49016dd95b9
devisingh dangi <devisinghdangi786@gmail.com>
2026-08-03 19:51:30 +0530

    api endpoints urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
@@ -27,3 +28,14 @@ urlpatterns = [
     path('seller/',include('seller.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Standard Swagger/ReDoc UIs
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui-alias'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc-alias'),
+    path('api/schema-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='schema-ui'),
+]
